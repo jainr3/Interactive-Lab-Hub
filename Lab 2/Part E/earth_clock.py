11b745bb15494d5cc238c3de3202d048bded6426
Rahul Jain <rj299@cornell.edu>
2022-09-10 01:41:30 -0400

    Added Part E Code and Files

---
+++ Lab 2/Part E/earth_clock.py
@@ -0,0 +1,169 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+
+import digitalio
+import board
+from PIL import Image, ImageDraw
+import adafruit_rgb_display.ili9341 as ili9341
+import adafruit_rgb_display.st7789 as st7789  # pylint: disable=unused-import
+import adafruit_rgb_display.hx8357 as hx8357  # pylint: disable=unused-import
+import adafruit_rgb_display.st7735 as st7735  # pylint: disable=unused-import
+import adafruit_rgb_display.ssd1351 as ssd1351  # pylint: disable=unused-import
+import adafruit_rgb_display.ssd1331 as ssd1331  # pylint: disable=unused-import
+
+
+driver_path = '/usr/lib/chromium-browser/chromedriver'
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+# browser is Chromium instead of Chrome
+#chrome_options.BinaryLocation = "/usr/bin/chromium-browser"
+# we use custom chromedriver for raspberry
+#driver_path = "/usr/bin/chromedriver"
+driver = webdriver.Chrome(driver_path, options=chrome_options)
+#driver = webdriver.Chrome(options=chrome_options, service=Service(driver_path))
+#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+print("Instantiated Chromium Drivers.")
+
+def update_image(driver):
+    driver.get('https://static.die.net/earth/mercator/2048.jpg')
+    print("Updating image and saving screenshot.")
+    time.sleep(20)
+
+    screenshot = driver.save_screenshot('earth.png')
+
+def crop_image(scale):
+    print(f"Cropping image at scale {scale}.")
+    im = Image.open('earth.png')
+    width, height = im.size
+    left = scale*300#10#80
+    top = scale*150#125#150
+    right = scale*700#305#230
+    bottom = scale*350#300#250
+
+    im1 = im.crop((left, top, right, bottom))
+    im1 = im1.convert('RGB')
+    im1 = im1.save("earth_cropped.jpg")
+
+# Configuration for CS and DC pins (these are PiTFT defaults):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# pylint: disable=line-too-long
+# Create the display:
+# disp = st7789.ST7789(spi, rotation=90,                            # 2.0" ST7789
+# disp = st7789.ST7789(spi, height=240, y_offset=80, rotation=180,  # 1.3", 1.54" ST7789
+# disp = st7789.ST7789(spi, rotation=90, width=135, height=240, x_offset=53, y_offset=40, # 1.14" ST7789
+# disp = hx8357.HX8357(spi, rotation=180,                           # 3.5" HX8357
+# disp = st7735.ST7735R(spi, rotation=90,                           # 1.8" ST7735R
+# disp = st7735.ST7735R(spi, rotation=270, height=128, x_offset=2, y_offset=3,   # 1.44" ST7735R
+# disp = st7735.ST7735R(spi, rotation=90, bgr=True,                 # 0.96" MiniTFT ST7735R
+# disp = ssd1351.SSD1351(spi, rotation=180,                         # 1.5" SSD1351
+# disp = ssd1351.SSD1351(spi, height=96, y_offset=32, rotation=180, # 1.27" SSD1351
+# disp = ssd1331.SSD1331(spi, rotation=180,                         # 0.96" SSD1331
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+# pylint: enable=line-too-long
+
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+
+def display_image_on_screen():
+    print("Displaying Image.")
+    # Create blank image for drawing.
+    # Make sure to create image with mode 'RGB' for full color.
+    if disp.rotation % 180 == 90:
+        height = disp.width  # we swap height/width to rotate it to landscape!
+        width = disp.height
+    else:
+        width = disp.width  # we swap height/width to rotate it to landscape!
+        height = disp.height
+    image = Image.new("RGB", (width, height))
+
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    disp.image(image)
+
+    image = Image.open("earth_cropped.jpg")
+    image = image.rotate(90)
+
+    # Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.Resampling.BICUBIC)
+
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+    # Display image.
+    disp.image(image)
+
+
+scale = 1.00
+update_image(driver)
+crop_image(scale)
+update = False
+last_fetched = time.time()
+display_image_on_screen()
+while True:
+    # Every 30 minutes update the fetched image
+    if (time.time() - last_fetched) > 30 * 60:
+        update_image(driver)
+        crop_image(scale)
+        update = True
+        last_fetched = time.time()
+
+    # Whenever buttonA is pressed, zoomOut; buttonB is zoomIn
+
+    if buttonB.value and not buttonA.value:  # just button A pressed
+        scale += 0.025
+        scale = min(scale, 1.20)
+        crop_image(scale)
+        update = True
+    if buttonA.value and not buttonB.value:  # just button B pressed
+        scale -= 0.025
+        scale = max(scale, 0.80)
+        crop_image(scale)
+        update = True
+
+    if (not buttonA.value and not buttonB.value) or update:  # none pressed
+        display_image_on_screen()
+        update = False
